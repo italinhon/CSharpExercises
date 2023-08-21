18954326104e9e7e03203f5f0928059407ef672b
Italo Siqueira <120528039+italinhon@users.noreply.github.com>
2023-08-21 20:34:58 -0300

    Testing_Ok

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2 { 
+    class Program { 
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre seu nome completo: ");
+            string fullName = Console.ReadLine();
+            Console.WriteLine("Quantos quarto tem na sua casa?");
+            int bedrooms = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com um preço de um produto: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha): ");
+            string[] vect = Console.ReadLine().Split(' ');
+            string lastName = vect[0];
+            int age = int.Parse(vect[1]);
+            double height = double.Parse(vect[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine(fullName);
+            Console.WriteLine(bedrooms);
+            Console.WriteLine(price.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+      
+        }
+    }
+}
