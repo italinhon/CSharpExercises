ab05e20632f3291493ed7167518128dda1bc0143
Italo Siqueira <120528039+italinhon@users.noreply.github.com>
2023-08-21 20:46:34 -0300

    Add files via upload

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio4_if_else {
+    internal class Program { 
+    
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre com um número: ");
+            
+            int N = int.Parse(Console.ReadLine());
+
+            if (N % 2 == 0) {
+                Console.WriteLine("Par");
+            }
+            else {
+                Console.WriteLine("Impar");
+            }
+        }
+    }
+
+}
