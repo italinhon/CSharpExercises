8c7aa223ba46bd8b8f669eda66b8b62a205076ef
Italo Siqueira <120528039+italinhon@users.noreply.github.com>
2023-08-21 20:40:58 -0300

    Add files via upload

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio3_if_else { 
+    internal class Program { 
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre com um número: ");
+            
+            int a = int.Parse(Console.ReadLine());
+
+            if (a < 0) {
+                Console.WriteLine("Negativo");
+            }
+            else {
+                Console.WriteLine("Não Negativo");
+
+            }
+        }    
+    }
+}
