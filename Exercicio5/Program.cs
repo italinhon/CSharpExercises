7f400801bfb13a50a02d92ecc8ed4123264733e1
Italo Siqueira <120528039+italinhon@users.noreply.github.com>
2023-08-21 20:49:00 -0300

    Add files via upload

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio5_while { 
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite a senha: ");
+
+            int x = int.Parse(Console.ReadLine());
+
+
+            while (x != 2002) {
+                Console.WriteLine("Senha Invalida");
+                x = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
